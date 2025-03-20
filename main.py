720b21d622b6054adec9ceff60403959c41e986b
development7777 <kodai819@outlook.com>
2025-03-20 12:27:59 +0900

    configファイルをyamlに 変更

--- main.py
+++ main.py
@@ -6,3 +6,20 @@
     This is the main file of the project.
 """
 
+# modules import
+
+import os
+import sys
+import yaml # config file
+
+# variables
+args = sys.argv
+
+# main
+
+def main():
+    # args check
+    
+    global args
+
+    # .bashprc.yaml file check
